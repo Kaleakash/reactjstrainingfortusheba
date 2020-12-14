61be2f59ba1be74a2fc0dfa3d2e1cb0647b20ab9
Akash <akash300383@gmail.com>
2020-12-14 09:53:56 +0530

    Redux-persist

--- VSCodePrograms/react-with-redux/src/index.js
+++ VSCodePrograms/react-with-redux/src/index.js
@@ -2,17 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
-import reducer from './Store/reducer'
+import {combineReducers, createStore} from 'redux';
+import reducer from './Store/reducer'   // single reducer 
 import {Provider} from 'react-redux'
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'   //web storage internally use localStorage 
+import { PersistGate } from 'redux-persist/integration/react';  // import 
+import {composeWithDevTools} from 'redux-devtools-extension';   // add dev tool module 
 
-const storeInfo = createStore(reducer);
+const persistConfiguration = {
+  key:"root",
+  storage:storage
+}
+
+/* const multReducer =      combineReducers({
+                    employeeReducer,productReducer
+        })*/
+
+const PersistReducer = persistReducer(persistConfiguration,reducer);
+
+const storeInfo = createStore(PersistReducer,composeWithDevTools());  // adding 
+
+const PersistStore = persistStore(storeInfo);
+
 
 ReactDOM.render(
   <React.StrictMode>
+
     <Provider store={storeInfo}>
-    <App />
+    
+    <PersistGate persistor={PersistStore}>
+          <App />
+    </PersistGate> 
+
     </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
